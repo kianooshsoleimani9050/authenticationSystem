841afd8f16e87e4782165ed3cf1d49a5c7360f3a
kianooshsoleimani9050 <mazyar.solo@gmail.com>
2022-01-20 09:34:56 +0330

    feat: :sparkles: A List of Users base on the api data
    when fetching data from api call we show a list of users that each one hs detail and a link to more detail

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,34 @@
+import { useFetchData } from "../../hooks/useFetchData";
+import SingleUserCard from "./home/SingleUserCard";
+
+const Home = () => {
+  const { result, loading, error } = useFetchData({
+    path: "/api/users",
+    query: "page=2",
+  });
+  return (
+    <div className="p-2 p-md-4 m-0 row">
+      {loading ? (
+        "Loading"
+      ) : (
+        <>
+          {result?.data?.map(({ id, email, first_name, last_name, avatar }) => (
+            <SingleUserCard
+              key={`singleUserCard${id}`}
+              col="col-12 col-sm-6 col-md-4 col-xl-3"
+              id={id}
+              email={email}
+              firstName={first_name}
+              lastName={last_name}
+              avatar={avatar}
+              path={"/user/" + id}
+            />
+          ))}
+        </>
+      )}
+      {error?.isError && <span className="text-danger">{error?.errorMsg}</span>}
+    </div>
+  );
+};
+
+export default Home;
