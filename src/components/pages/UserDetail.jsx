5378e55ee6d4dc30e15a4c67e121d0c24c8dfb4f
kianooshsoleimani9050 <mazyar.solo@gmail.com>
2022-01-20 09:37:25 +0330

    feat: :sparkles: a component for user detail page
    this component is fetching data base on id of user and render a ui base on that data

---
+++ src/components/pages/UserDetail.jsx
@@ -0,0 +1,28 @@
+import { useParams } from "react-router-dom";
+import { useFetchData } from "../../hooks/useFetchData";
+import SingleUserCard from "./home/SingleUserCard";
+const UserDetail = () => {
+  const { id } = useParams();
+  const { result, loading, error } = useFetchData({
+    path: "/api/users/" + id,
+  });
+  return (
+    <div className="p-0 m-0 row align-items-center justify-content-center">
+      {loading ? (
+        "Loading"
+      ) : (
+        <SingleUserCard
+          key={`singleUserCard${id}`}
+          col="col-12 col-md-4 col-xl-3"
+          id={id}
+          email={result?.data?.email}
+          firstName={result?.data?.first_name}
+          lastName={result?.data?.last_name}
+          avatar={result?.data?.avatar}
+        />
+      )}
+    </div>
+  );
+};
+
+export default UserDetail;
